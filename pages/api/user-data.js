153cd1cca45c7acb1902baf9b88e39eda8d036c4
krishomni <omnijyothi@gmail.com>
2025-09-29 19:02:59 -0400

    Frontend setup

--- pages/api/user-data.js
+++ pages/api/user-data.js
@@ -1,22 +1,22 @@
-// pages/api/user-data.js
 import clientPromise from '../../lib/mongodb'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       const client = await clientPromise
-      const db = client.db('tests') 
+      const db = client.db('test') // Replace with your database name
       
-      // Fetch from MongoDB instead of JSON file
+      // Fetch the user data document from MongoDB
       const userData = await db.collection('settings').findOne({ _id: 'userData' })
       
       if (!userData) {
         return res.status(404).json({ message: 'User data not found' })
       }
 
-      console.log('API: Returning data from MongoDB')
-      console.log('Services count:', userData.services?.length)
-      console.log('First service has image:', !!userData.services?.[0]?.image)
+      console.log('MongoDB API: Data loaded')
+      console.log('Gallery exists:', !!userData.gallery)
+      console.log('Facility images:', userData.gallery?.facilityImages?.length || 0)
+      console.log('Before/After cases:', userData.gallery?.beforeAfterCases?.length || 0)
       
       // Remove MongoDB _id from response
       const { _id, ...dataWithoutId } = userData
