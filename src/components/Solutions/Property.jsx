e96767ec38acb471ab18d1d9118ed3b16c9f94ad
rimma <32555704+rmmshv@users.noreply.github.com>
2025-10-01 14:02:45 -0700

    added solutions page

---
+++ src/components/Solutions/Property.jsx
@@ -0,0 +1,7 @@
+export default function Farmers() {
+  return (
+    <div>
+      <h1>Property Solution</h1>
+    </div>
+  );
+}
---
+++ src/components/Solutions/Vendors.jsx
@@ -0,0 +1,7 @@
+export default function Vendors() {
+  return (
+    <div>
+      <h1>Vendors Solution</h1>
+    </div>
+  );
+}
