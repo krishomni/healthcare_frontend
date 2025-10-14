ad3ccfe3b243e9591f555c92f236e2fc712cde37
danni-nu <145931898+danni-nu@users.noreply.github.com>
2025-10-14 14:34:46 -0400

    tests for the ticketing system frontend

---
+++ setupTests.js
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
+
+if (!Element.prototype.scrollBy) {
+  Element.prototype.scrollBy = function () {};
+}
